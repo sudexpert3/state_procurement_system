c8d5c09a4cbf178e60588e579eb1dc54aba6f03a
nnesteruk <nikita.nesteruck57@gmail.com>
2026-06-10 11:36:56 +0300

    add openapi-fetch and client

--- frontend/src/app/main.tsx
+++ frontend/src/app/main.tsx
@@ -1,38 +1,14 @@
 import "./global.css";
 
-import { StrictMode, Suspense } from "react";
+import { StrictMode } from "react";
 
-import { QueryClientProvider } from "@tanstack/react-query";
 import { createRoot } from "react-dom/client";
-import { ErrorBoundary } from "react-error-boundary";
 import { RouterProvider } from "react-router";
-
-import { queryClient } from "@/shared/api/query-client";
-import { Button } from "@/shared/ui/kit/button";
 
 import { router } from "./router";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <ErrorBoundary
-      // onReset={reset}
-      fallbackRender={({ resetErrorBoundary }) => (
-        <div className="flex flex-col items-center gap-4">
-          <h1>Произошла ошибка!</h1>
-          <Button
-            onClick={() => {
-              resetErrorBoundary();
-              window.location.assign(window.location.origin);
-            }}>
-            Перезагрузить
-          </Button>
-        </div>
-      )}>
-      <QueryClientProvider client={queryClient}>
-        <Suspense fallback={<div>Loading...</div>}>
-          <RouterProvider router={router} />
-        </Suspense>
-      </QueryClientProvider>
-    </ErrorBoundary>
+    <RouterProvider router={router} />
   </StrictMode>,
 );
